c1611b94ef412171382eec9ae3347632f972a38a
TCashion <58757351+TCashion@users.noreply.github.com>
2020-05-19 15:28:45 -0600

    add styling wth bootstrap

--- models/skill.js
+++ models/skill.js
@@ -1,16 +1,16 @@
 // set up data as an array of objects
 const skills = [
-    {id: 0, name: "HTML", learnedAt: "General Assembly"},
-    {id: 1, name: "CSS", learnedAt: "General Assembly"},
-    {id: 2, name: "JavaScript", learnedAt: "General Assembly"},
-    {id: 3, name: "jQuery", learnedAt: "General Assembly"},
-    {id: 4, name: "Firebase", learnedAt: "Self-Taught"},
-    {id: 5, name: "Bootstrap", learnedAt: "Self-Taught"},
-    {id: 6, name: "Node", learnedAt: "General Assembly"},
-    {id: 7, name: "Express", learnedAt: "General Assembly"},
-    {id: 8, name: "Git", learnedAt: "General Assembly"},
-    {id: 9, name: "Code Review", learnedAt: "General Assembly"},
-]
+    {id: 0, name: "HTML", learnedAt: "General Assembly", furtherInfo: "HTML is like the 'scaffolding' for web pages."},
+    {id: 1, name: "CSS", learnedAt: "General Assembly", furtherInfo: "CSS gives webpages style."},
+    {id: 2, name: "JavaScript", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 3, name: "jQuery", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 4, name: "Firebase", learnedAt: "Self-Taught", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 5, name: "Bootstrap", learnedAt: "Self-Taught", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 6, name: "Node", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 7, name: "Express", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 8, name: "Git", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+    {id: 9, name: "Code Review", learnedAt: "General Assembly", furtherInfo: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos amet non rerum ab sapiente vero tempora porro natus expedita corporis aspernatur tenetur, culpa quaerat quasi nihil sequi rem inventore. Autem."},
+];
 
 // export the functions as methods of the export object
 module.exports = {
@@ -25,3 +25,4 @@ function getAll() {
 function getOne(id) {
     return skills.find((skill) => skill.id === parseInt(id));
 };
+
